1fabfb32e43d85bae640aa4b3b71adc683cc2681
Konrad Goławski <konrad.golawski@gmail.com>
2016-01-28 11:40:08 +0100

    Tests to new methods

--- tests/PointTest.php
+++ tests/PointTest.php
@@ -26,4 +26,30 @@ class PointTest extends \PHPUnit_Framework_TestCase
                                     ->getLabel(),
                             $point->getLabel());
     }
+
+    public function testCoordinates()
+    {
+        $point = new \Algorithms\GraphTools\Point(1);
+
+        $this->assertNull($point->getX());
+        $this->assertEquals($point->setX(15), $point);
+        $this->assertEquals($point->getX(15), 15);
+
+        $this->assertNull($point->getY());
+        $this->assertEquals($point->setY(29), $point);
+        $this->assertEquals($point->getY(29), 29);
+    }
+
+    public function testToArrayMethod()
+    {
+        $point = new \Algorithms\GraphTools\Point(1);
+
+        $this->assertTrue(is_array($point->toArray()));
+        $this->assertEquals($point->toArray()['id'], 1);
+
+        $point->setX(0)
+              ->setY(-5);
+        $this->assertEquals($point->toArray()['x'], 0);
+        $this->assertEquals($point->toArray()['y'], -5);
+    }
 }
