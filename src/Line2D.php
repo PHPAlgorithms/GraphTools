2babb6e664c15a25e4abb56da7c33c4dd1ab926f
Konrad Goławski <konrad.golawski@gmail.com>
2016-07-05 16:49:57 +0200

    Line2D doc comments

--- src/Line2D.php
+++ src/Line2D.php
@@ -1,11 +1,26 @@
 <?php
 
+/**
+ * @author ventaquil <ventaquil@outlook.com>
+ * @licence MIT
+ */
+
 namespace PHPAlgorithms\GraphTools;
 
 use PHPAlgorithms\GraphTools\Interfaces\LineInterface;
 use PHPAlgorithms\GraphTools\Exceptions\LineException;
 
+/**
+ * Class Line2D
+ * @package PHPAlgorithms\GraphTools
+ */
 class Line2D extends Line1D implements LineInterface {
+    /**
+     * Method checks sent variable and throws LineException if it is not an Point2D instance.
+     *
+     * @param mixed $point Variable to check.
+     * @throws LineException If sent argument is not an Point2D instance.
+     */
     protected function checkPoint($point)
     {
         if (!($point instanceof Point2D)) {
@@ -13,11 +28,22 @@ class Line2D extends Line1D implements LineInterface {
         }
     }
 
+    /**
+     * Method counts width.
+     *
+     * return float Line width.
+     */
     protected function countWidth()
     {
         return sqrt(pow($this->from->x - $this->to->x, 2) + pow($this->from->y - $this->to->y, 2));
     }
 
+    /**
+     * Line2D constructor.
+     *
+     * @param mixed $from First end of the line.
+     * @param mixed $to Second end of the line.
+     */
     public function __construct($from, $to)
     {
         parent::__construct($from, $to);
