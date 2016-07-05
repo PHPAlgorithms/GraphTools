a82967a1316c691c6dc8e4b124d38dec6472689c
Konrad Goławski <konrad.golawski@gmail.com>
2016-07-05 16:56:48 +0200

    Point1D doc comments

--- src/Point1D.php
+++ src/Point1D.php
@@ -1,13 +1,32 @@
 <?php
 
+/**
+ * @author ventaquil <ventaquil@outlook.com>
+ * @licence MIT
+ */
+
 namespace PHPAlgorithms\GraphTools;
 
 use PHPAlgorithms\GraphTools\Interfaces\PointInterface;
 use PHPAlgorithms\GraphTools\Exceptions\PointException;
 
+/**
+ * Class Point1D
+ * @package PHPAlgorithms\GraphTools
+ * @property-read int|float $x
+ */
 class Point1D extends AbstractPoint implements PointInterface {
+    /**
+     * @var int|float $x Position on OX axis.
+     */
     protected $x;
 
+    /**
+     * Method checks position on OX axis.
+     *
+     * @param int|float $x Position on OX axis.
+     * @throws PointException When $x is not a numeric value.
+     */
     protected function checkX($x)
     {
         if (!is_numeric($x)) {
@@ -15,6 +34,11 @@ class Point1D extends AbstractPoint implements PointInterface {
         }
     }
 
+    /**
+     * Point1D constructor.
+     *
+     * @param int|float $x Position on OX axis.
+     */
     public function __construct($x)
     {
         $this->checkX($x);
@@ -22,6 +46,12 @@ class Point1D extends AbstractPoint implements PointInterface {
         $this->x = $x;
     }
 
+    /**
+     * Move point by sent value.
+     *
+     * @param int|float $x Move vector.
+     * @return Point1D Method returns current object instance.
+     */
     public function move($x = 0)
     {
         $this->checkX($x);
@@ -31,6 +61,11 @@ class Point1D extends AbstractPoint implements PointInterface {
         return $this;
     }
 
+    /**
+     * Method creates point in lower dimension.
+     *
+     * @return AbstractPoint Returns an abstract point.
+     */
     public function lowerDimension()
     {
         return new AbstractPoint();
