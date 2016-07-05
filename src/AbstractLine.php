2caaab842d0a3968698c8cdc90259071186883a5
Konrad Goławski <konrad.golawski@gmail.com>
2016-07-05 16:38:48 +0200

    AbstractLine doc comments

--- src/AbstractLine.php
+++ src/AbstractLine.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @author ventaquil <ventaquil@outlook.com>
+ * @licence MIT
+ */
+
 namespace PHPAlgorithms\GraphTools;
 
 use PHPAlgorithms\GraphTools\Interfaces\LineInterface;
@@ -7,11 +12,28 @@ use PHPAlgorithms\GraphTools\Exceptions\LineException;
 use PHPAlgorithms\GraphTools\Traits\MagicGet;
 use PHPAlgorithms\GraphTools\Traits\toArray;
 
+/**
+ * Class AbstractLine
+ * @package PHPAlgorithms\GraphTools
+ * @property-read Point $from
+ * @property-read Point $to
+ */
 class AbstractLine implements LineInterface {
     use MagicGet, toArray;
 
+    /**
+     * @var Point $from First end of the line.
+     * @var Point $to Second end of the line.
+     */
     protected $from, $to;
 
+    /**
+     * Method checks send arguments and throws exception if it is the same point.
+     *
+     * @param mixed $from First variable to check.
+     * @param mixed $to Second variable to check.
+     * @throws LineException When $from and $to is the same object.
+     */
     protected function checkPoints($from, $to)
     {
         if ($from->equals($to)) {
@@ -19,6 +41,12 @@ class AbstractLine implements LineInterface {
         }
     }
 
+    /**
+     * Method checks sent variable and throws LineException if it is not an AbstractPoint instance.
+     *
+     * @param mixed $point Variable to check.
+     * @throws LineException If sent argument is not an AbstractPoint instance.
+     */
     protected function checkPoint($point)
     {
         if (!($point instanceof AbstractPoint)) {
@@ -26,24 +54,40 @@ class AbstractLine implements LineInterface {
         }
     }
 
+    /**
+     * Set first end of the line.
+     *
+     * @param AstractPoint $from First end of the line.
+     */
     protected function setFrom($from)
     {
         $this->checkPoint($from);
         $this->from = $from;
     }
 
+    /**
+     * Set second end of the line.
+     *
+     * @param AstractPoint $to Second end of the line.
+     */
     protected function setTo($to)
     {
         $this->checkPoint($to);
         $this->to = $to;
     }
 
+    /**
+     * Abstract Line constructor.
+     *
+     * @param AbstractPoint $from First end of the line.
+     * @param AbstractPoint $to Second end of the line.
+     */
     public function __construct($from, $to)
     {
+        $this->checkPoints($from, $to);
+
         $this->setFrom($from);
 
         $this->setTo($to);
-
-        $this->checkPoints($from, $to);
     }
 }
